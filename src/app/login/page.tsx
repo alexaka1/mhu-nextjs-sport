8ad12b4d57a773c8281ca2f506865a5d96a6a895
Martossy Alex <github@mail.martossy.hu>
2024-04-05 14:27:22 +0200

    Add nextauth with github

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,14 +1,18 @@
-import { Metadata } from 'next';
-import UnderConstruction from '@/app/ui/under-construction';
-
-export const metadata: Metadata = {
-  title: `Bejelentkezés`,
-};
+'use client';
+import { signIn } from 'next-auth/react';
 
 export default function Home() {
   return (
     <>
-      <UnderConstruction />
+      <button
+        onClick={() => signIn('github')}
+        className={`rounded-md px-3.5 py-2.5 text-sm font-semibold shadow-sm transition-colors
+            duration-200
+            ease-in-out bg-primary text-bg-contrast hover:bg-primary-600 focus-visible:outline focus-visible:outline-2
+            focus-visible:outline-offset-2 focus-visible:outline-primary-600 active:bg-primary-800`}
+      >
+        Github login
+      </button>
     </>
   );
 }
