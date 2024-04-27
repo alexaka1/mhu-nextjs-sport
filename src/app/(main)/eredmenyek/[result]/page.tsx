d30b9069b515e963c5932997433d909d948f4372
Martossy Alex <github@mail.martossy.hu>
2024-04-27 18:01:08 +0200

    Reworked results to be more server side component

--- src/app/(main)/eredmenyek/[result]/page.tsx
+++ src/app/(main)/eredmenyek/[result]/page.tsx
@@ -3,6 +3,7 @@ import ResultsTab from '@/app/ui/results/results-tab';
 import { Suspense } from 'react';
 import Skeleton from '@/app/ui/skeleton';
 import { canEditResults, getResults } from '@/app/lib/actions';
+import { Result } from '@/app/lib/types';
 
 // export const dynamic = 'force-dynamic';
 
@@ -10,9 +11,15 @@ export const metadata: Metadata = {
   title: 'Eredmények',
 };
 
-export default async function Page() {
+export default async function Page({
+  params,
+}: {
+  params?: {
+    result?: string;
+  };
+}) {
   const canEdit = await canEditResults();
-  const sportag = 'Labdarúgás';
+  const sportag = decodeURIComponent(params?.result || ('Labdarúgás' as Result));
   const results = await getResults(sportag);
   return (
     <main className="flex flex-col items-center justify-center bg-white dark:bg-gray-800">
