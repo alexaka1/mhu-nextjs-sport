62888dad40fd1c335b32367f346cf28a8e6cdb9c
Martossy Alex <github@mail.martossy.hu>
2024-05-04 19:22:59 +0200

    Better dark mode toggle initial value

--- src/app/ui/dark-mode-toggle.tsx
+++ src/app/ui/dark-mode-toggle.tsx
@@ -3,17 +3,26 @@
 import { useTheme } from 'next-themes';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+import { useEffect } from 'react';
 
 export default function DarkModeToggle() {
   const { systemTheme, theme, setTheme } = useTheme();
   const currentTheme = theme === 'system' ? systemTheme : theme;
+  useEffect(() => {
+    if (currentTheme === 'dark') {
+      const toggle = document.getElementById('dark-mode-toggle') as HTMLInputElement | null;
+      if (toggle && !toggle.checked) {
+        toggle.click();
+      }
+    }
+  }, []);
   return (
-    <label className="dui-swap dui-swap-rotate" title={theme === 'dark' ? 'Világos mód' : 'Sötét mód'}>
+    <label className="dui-swap dui-swap-rotate" title={'Sötét/Világos mód'}>
       <input
+        id={'dark-mode-toggle'}
         type="checkbox"
-        className="hidden"
+        className="sr-only"
         value="dark"
-        checked={currentTheme === 'dark'}
         onChange={(v) => {
           if (v.target.checked) {
             setTheme('dark');
