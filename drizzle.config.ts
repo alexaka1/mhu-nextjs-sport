28aff89a1ddbac460dbc4245f1640ed2221a7100
Martossy Alex <github@mail.martossy.hu>
2025-01-19 13:05:16 +0100

    Fix drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,8 +4,7 @@ import { env } from '@/app/lib/env';
 export default {
   schema: './src/app/db/schema.ts',
   out: './drizzle',
-  driver: 'turso',
-  dialect: 'sqlite',
+  dialect: 'turso',
   dbCredentials: {
     url: env.TURSO_DATABASE_URL,
     authToken: env.TURSO_AUTH_TOKEN,
