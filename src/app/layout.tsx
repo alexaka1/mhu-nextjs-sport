bf1e992bcb4db7cd39690ae307e82be067440936
Martossy Alex <github@mail.martossy.hu>
2025-02-23 16:21:35 +0100

    Try add daisyui
    geist not working yet
    *-contrast not working yet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 
 import { GeistSans } from 'geist/font/sans';
-
 import { GeistMono } from 'geist/font/mono';
 import { type ReactNode } from 'react';
 import './globals.css';
