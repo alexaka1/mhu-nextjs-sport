f4f849963329e230096549ab742a362eb24a1217
Martossy Alex <github@mail.martossy.hu>
2025-03-29 10:23:22 +0100

    Update drizzle code

--- src/app/db/db.ts
+++ src/app/db/db.ts
@@ -1,10 +1,9 @@
-import { createClient } from '@libsql/client';
 import { drizzle } from 'drizzle-orm/libsql';
-import * as schema from './schema';
 import { env } from '@/app/lib/env';
 
-export const client = createClient({
-  url: env.TURSO_DATABASE_URL,
-  authToken: env.TURSO_AUTH_TOKEN,
+export const db = drizzle({
+  connection: {
+    url: env.TURSO_DATABASE_URL,
+    authToken: env.TURSO_AUTH_TOKEN,
+  },
 });
-export const db = drizzle(client, { schema: schema });
