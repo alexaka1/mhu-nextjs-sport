3d8d0951a35b4a459e3fb0fc2684d94c65697343
Martossy Alex <github@mail.martossy.hu>
2025-04-05 12:48:46 +0200

    Add sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,45 +1,77 @@
 import { type MetadataRoute } from 'next';
 import { Result } from '@/app/lib/types';
 
+const years: Array<{ year: number; pages: MetadataRoute.Sitemap }> = [
+  {
+    year: 2025,
+    pages: [
+      {
+        url: 'https://sport.martossy.hu/2025',
+        lastModified: new Date(),
+        changeFrequency: 'daily',
+      },
+    ],
+  },
+  {
+    year: 2024,
+    pages: [
+      {
+        url: 'https://sport.martossy.hu/2024',
+        lastModified: new Date(),
+        changeFrequency: 'never',
+      },
+      {
+        url: 'https://sport.martossy.hu/2024/koszonto',
+        lastModified: new Date(),
+        changeFrequency: 'never',
+      },
+      {
+        url: 'https://sport.martossy.hu/2024/sportagak',
+        lastModified: new Date(),
+        changeFrequency: 'never',
+      },
+      {
+        url: 'https://sport.martossy.hu/2024/helyszinek',
+        lastModified: new Date(),
+        changeFrequency: 'never',
+      },
+      {
+        url: 'https://sport.martossy.hu/2024/szallas',
+        lastModified: new Date(),
+        changeFrequency: 'never',
+      },
+      {
+        url: 'https://sport.martossy.hu/2024/programok',
+        lastModified: new Date(),
+        changeFrequency: 'never',
+      },
+      {
+        url: 'https://sport.martossy.hu/2024/eredmenyek',
+        lastModified: new Date(),
+        changeFrequency: 'never',
+      },
+    ],
+  },
+];
+
+// this page is static, so new Date() will be the time of last deployment
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: 'https://sport.martossy.hu/',
       lastModified: new Date(),
       priority: 1,
+      changeFrequency: 'monthly',
     },
-    {
-      url: 'https://sport.martossy.hu/koszonto',
-      lastModified: new Date(),
-    },
-    {
-      url: 'https://sport.martossy.hu/helyszinek',
-      lastModified: new Date(),
-    },
-    {
-      url: 'https://sport.martossy.hu/programok',
-      lastModified: new Date(),
-    },
-    {
-      url: 'https://sport.martossy.hu/sportagak',
-      lastModified: new Date(),
-    },
-    {
-      url: 'https://sport.martossy.hu/szallas',
-      lastModified: new Date(),
-    },
-    {
-      url: 'https://sport.martossy.hu/eredmenyek',
-      lastModified: new Date(),
-    },
-    ...results(),
+    ...years.flatMap(({ pages }) => pages),
+    ...results2024(),
   ];
 }
 
-function results(): MetadataRoute.Sitemap {
+function results2024(): MetadataRoute.Sitemap {
   return Result.options.map((result) => ({
-    url: `https://sport.martossy.hu/eredmenyek/${encodeURIComponent(result)}`,
+    url: `https://sport.martossy.hu/2024/eredmenyek/${encodeURIComponent(result)}`,
     lastModified: new Date(),
-    changeFrequency: 'hourly',
+    changeFrequency: 'never',
   }));
 }
