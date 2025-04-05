a3f8f49ee986e92012f7cee2f51d56624cb9a922
Martossy Alex <github@mail.martossy.hu>
2025-04-05 12:30:51 +0200

    Add 2025 landing page

---
+++ src/app/(years)/2025/page.tsx
@@ -0,0 +1,25 @@
+import { type Metadata } from 'next';
+import { APP_DEFAULT_TITLE } from '@/app/manifest';
+import LandingLinks from '@/app/ui/landing/2025/landing-links';
+import LandingBackground from '@/app/ui/landing/2025/landing-background';
+import LandingTitle from '@/app/ui/landing/2025/landing-title';
+
+export const metadata: Metadata = {
+  title: `2025 - Békéscsaba | ${APP_DEFAULT_TITLE}`,
+};
+
+export default function Home() {
+  return (
+    <>
+      <main className="relative flex min-h-svh flex-col items-center justify-between gap-1 p-4 md:p-24">
+        <LandingBackground />
+        <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex"></div>
+
+        <LandingTitle />
+        <div className="md:pb-32 xl:pb-0">
+          <LandingLinks />
+        </div>
+      </main>
+    </>
+  );
+}
