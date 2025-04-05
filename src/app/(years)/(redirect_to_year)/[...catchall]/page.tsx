c906fec53328183e68df5c7d2da119ca7c90244a
Martossy Alex <github@mail.martossy.hu>
2025-04-05 17:42:23 +0200

    Refactor

--- src/app/(years)/(redirect_to_year)/[...catchall]/page.tsx
+++ src/app/(years)/(redirect_to_year)/[...catchall]/page.tsx
@@ -1,9 +1,9 @@
 import { redirect, RedirectType } from 'next/navigation';
-import { currentYear } from '@/app/(years)/(redirect_to_year)/current-year';
+import { currentYear } from '../current-year';
 
 import NotFound from '@/app/not-found';
 
-export default async function RedirectToCurrentYear({ params }: { params: Promise<{ catchall: string[] }> }) {
+export default async function FallbackToCurrentYear({ params }: { params: Promise<{ catchall: string[] }> }) {
   const { catchall } = await params;
   if (catchall.length === 0) {
     redirect(`/`, RedirectType.replace);
