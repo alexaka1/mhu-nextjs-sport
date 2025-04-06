c6ed5fcab89dbecf21e2c60de9d31f8f009c953f
Martossy Alex <github@mail.martossy.hu>
2025-04-06 10:18:08 +0200

    Simplify landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,3 @@
-import { type Metadata } from 'next';
-import { APP_DEFAULT_TITLE } from '@/app/manifest';
-import LandingLinks from '@/app/ui/landing/2024/landing-links';
-import LandingBackground from '@/app/ui/landing/2024/landing-background';
-import LandingTitle from '@/app/ui/landing/2024/landing-title';
-
-export const metadata: Metadata = {
-  title: `2024 - Nyíregyháza | ${APP_DEFAULT_TITLE}`,
-};
-
-export default function Home() {
-  return (
-    <>
-      <main className="relative flex min-h-svh flex-col items-center justify-between gap-1 p-4 md:p-24">
-        <LandingBackground />
-        <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex"></div>
-
-        <LandingTitle />
-        <div className="md:pb-32 xl:pb-0">
-          <LandingLinks />
-        </div>
-      </main>
-    </>
-  );
-}
+import Home from '@/app/(years)/2024/page';
+export { metadata } from '@/app/(years)/2024/page';
+export default Home;
