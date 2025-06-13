dd67e6ee90b3bc741a1e60c89c4d8dc241bda93b
Alex Martossy <github@mail.martossy.hu>
2025-06-13 18:16:52 +0200

    Add menu bar to sports

--- src/app/(years)/2025/(main)/sportagak/menu.tsx
+++ src/app/(years)/2025/(main)/sportagak/menu.tsx
@@ -13,10 +13,56 @@ import {
 
 export const tabs = [
   {
+    title: 'Asztalitenisz [női, férfi]',
+    icon: (
+      <IconPingPong
+        className="me-2 size-4 text-gray-400 group-data-[hover]:text-secondary-600 group-data-[selected]:text-primary dark:text-gray-500 dark:group-data-[hover]:text-gray-300 group-data-[selected]:dark:text-primary-400"
+        aria-hidden="true"
+        stroke={1.5}
+      />
+    ),
+  },
+  {
+    title: 'Csocsó',
+    icon: (
+      <IconSoccerField
+        className="me-2 size-4 text-gray-400 group-data-[hover]:text-secondary-600 group-data-[selected]:text-primary dark:text-gray-500 dark:group-data-[hover]:text-gray-300 group-data-[selected]:dark:text-primary-400"
+        aria-hidden="true"
+      />
+    ),
+  },
+  {
+    title: 'Darts',
+    icon: (
+      <IconTargetArrow
+        className="me-2 size-4 text-gray-400 group-data-[hover]:text-secondary-600 group-data-[selected]:text-primary dark:text-gray-500 dark:group-data-[hover]:text-gray-300 group-data-[selected]:dark:text-primary-400"
+        aria-hidden="true"
+      />
+    ),
+  },
+  {
+    title: 'Férfi labdarúgás',
+    icon: (
+      <IconPlayFootball
+        className="me-2 size-4 text-gray-400 group-data-[hover]:text-secondary-600 group-data-[selected]:text-primary dark:text-gray-500 dark:group-data-[hover]:text-gray-300 group-data-[selected]:dark:text-primary-400"
+        aria-hidden="true"
+      />
+    ),
+  },
+  {
+    title: 'Íjászat',
+    icon: (
+      <IconBow
+        className="me-2 size-4 text-gray-400 group-data-[hover]:text-secondary-600 group-data-[selected]:text-primary dark:text-gray-500 dark:group-data-[hover]:text-gray-300 group-data-[selected]:dark:text-primary-400"
+        aria-hidden="true"
+      />
+    ),
+  },
+  {
     title: 'Női és férfi síkfutás',
     icon: (
       <IconRun
-        className={`me-2 size-4 text-gray-400 group-hover:text-secondary-600 group-data-active:text-primary dark:text-gray-500 dark:group-hover:text-gray-300 group-data-active:dark:text-primary-400`}
+        className={`me-2 size-4 text-gray-400 group-data-[hover]:text-secondary-600 group-data-[selected]:text-primary dark:text-gray-500 dark:group-data-[hover]:text-gray-300 group-data-[selected]:dark:text-primary-400`}
         aria-hidden="true"
         stroke={1.5}
       />
@@ -26,19 +72,18 @@ export const tabs = [
     title: 'Női és férfi súlylökés',
     icon: (
       <IconBallVolleyball
-        className="me-2 size-4 text-gray-400 group-hover:text-secondary-600 group-data-active:text-primary dark:text-gray-500 dark:group-hover:text-gray-300 group-data-active:dark:text-primary-400"
+        className="me-2 size-4 text-gray-400 group-data-[hover]:text-secondary-600 group-data-[selected]:text-primary dark:text-gray-500 dark:group-data-[hover]:text-gray-300 group-data-[selected]:dark:text-primary-400"
         aria-hidden="true"
         stroke={1.5}
       />
     ),
   },
   {
-    title: 'Asztalitenisz [női, férfi]',
+    title: 'Női kosárlabda',
     icon: (
-      <IconPingPong
-        className="me-2 size-4 text-gray-400 group-hover:text-secondary-600 group-data-active:text-primary dark:text-gray-500 dark:group-hover:text-gray-300 group-data-active:dark:text-primary-400"
+      <IconBallBasketball
+        className="me-2 size-4 text-gray-400 group-data-[hover]:text-secondary-600 group-data-[selected]:text-primary dark:text-gray-500 dark:group-data-[hover]:text-gray-300 group-data-[selected]:dark:text-primary-400"
         aria-hidden="true"
-        stroke={1.5}
       />
     ),
   },
@@ -46,7 +91,7 @@ export const tabs = [
     title: 'Tollaslabda',
     icon: (
       <IconFeather
-        className="me-2 size-4 text-gray-400 group-hover:text-secondary-600 group-data-active:text-primary dark:text-gray-500 dark:group-hover:text-gray-300 group-data-active:dark:text-primary-400"
+        className="me-2 size-4 text-gray-400 group-data-[hover]:text-secondary-600 group-data-[selected]:text-primary dark:text-gray-500 dark:group-data-[hover]:text-gray-300 group-data-[selected]:dark:text-primary-400"
         aria-hidden="true"
         stroke={1.5}
       />
@@ -56,52 +101,7 @@ export const tabs = [
     title: 'Úszás',
     icon: (
       <IconSwimming
-        className="me-2 size-4 text-gray-400 group-hover:text-secondary-600 group-data-active:text-primary dark:text-gray-500 dark:group-hover:text-gray-300 group-data-active:dark:text-primary-400"
-        aria-hidden="true"
-      />
-    ),
-  },
-  {
-    title: 'Férfi labdarúgás',
-    icon: (
-      <IconPlayFootball
-        className="me-2 size-4 text-gray-400 group-hover:text-secondary-600 group-data-active:text-primary dark:text-gray-500 dark:group-hover:text-gray-300 group-data-active:dark:text-primary-400"
-        aria-hidden="true"
-      />
-    ),
-  },
-  {
-    title: 'Csocsó',
-    icon: (
-      <IconSoccerField
-        className="me-2 size-4 text-gray-400 group-hover:text-secondary-600 group-data-active:text-primary dark:text-gray-500 dark:group-hover:text-gray-300 group-data-active:dark:text-primary-400"
-        aria-hidden="true"
-      />
-    ),
-  },
-  {
-    title: 'Darts',
-    icon: (
-      <IconTargetArrow
-        className="me-2 size-4 text-gray-400 group-hover:text-secondary-600 group-data-active:text-primary dark:text-gray-500 dark:group-hover:text-gray-300 group-data-active:dark:text-primary-400"
-        aria-hidden="true"
-      />
-    ),
-  },
-  {
-    title: 'Íjászat',
-    icon: (
-      <IconBow
-        className="me-2 size-4 text-gray-400 group-hover:text-secondary-600 group-data-active:text-primary dark:text-gray-500 dark:group-hover:text-gray-300 group-data-active:dark:text-primary-400"
-        aria-hidden="true"
-      />
-    ),
-  },
-  {
-    title: 'Női kosárlabda',
-    icon: (
-      <IconBallBasketball
-        className="me-2 size-4 text-gray-400 group-hover:text-secondary-600 group-data-active:text-primary dark:text-gray-500 dark:group-hover:text-gray-300 group-data-active:dark:text-primary-400"
+        className="me-2 size-4 text-gray-400 group-data-[hover]:text-secondary-600 group-data-[selected]:text-primary dark:text-gray-500 dark:group-data-[hover]:text-gray-300 group-data-[selected]:dark:text-primary-400"
         aria-hidden="true"
       />
     ),
