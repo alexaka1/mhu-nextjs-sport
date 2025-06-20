e3ab945742310a952e8bbeb3b524ae9350277e9e
Alex Martossy <github@mail.martossy.hu>
2025-06-20 17:34:03 +0200

    Update "hasznos informaciok" page for 2025 with detailed location, schedule, and venue adjustments

--- src/app/(years)/2025/(main)/hasznos-informaciok/page.tsx
+++ src/app/(years)/2025/(main)/hasznos-informaciok/page.tsx
@@ -31,20 +31,29 @@ export default function Page() {
               Békéscsaba, Bajza utca 33. szám
             </a>
             . Megközelítése: a Bajza utcáról lekanyarodva, az egyetem főépülete mögötti úton végig haladva lehetséges.
-            Az út végén jobbról található az Alma Mater Hostel, a bejelentkezés helyszíne.
+            Az út közepén balra a Szent-Györgyi Albert Technikum [Szegya], míg a végén balra található az Alma Mater
+            Hostel, ez a bejelentkezés két helyszíne.
           </p>
           <Title id={`Helyszínek`}>
             <span data-counter>Helyszínek</span>
           </Title>
           <p>
-            A csapatok szállása az egyetem Alma Mater Hostelében [Bajza utca 33. szám], illetve a szomszédos
-            Szent-Györgyi Albert Technikum kollégiumában [Gyulai út 53. szám] lesz. Az ünnepélyes megnyitóra és a
-            záróünnepségre is itt, a belső udvaron kerül sor.
+            A csapatok szállása az egyetem Alma Mater Hostelében [Bajza utca 33. szám], illetve a szomszédos Szegya
+            kollégiumában [Gyulai út 53. szám] lesz 2,3 és 4 ágyas, fürdőszobás szobákban.
           </p>
           <p>
-            Étkezés, főügyészi verseny és esti bulik helyszíne a Csaba Park Pálinkamúzeum [Gyulai út 61/2. szám], ami a
-            szállástól a belső udvaron át megközelíthető pár perces sétával. A tájékozódást eligazító táblák segítik
-            majd.
+            Szállásbeosztás: az Alma Mater Hostelben kerül elszállásolásra Bács, Békés, Szolnok, LegfÜ és Pest. A
+            Szegyában kap szállást Borsod, Csongrád, Főváros, Hajdú, Heves-Nógrád, KNYF és Szabolcs. A VIP szállások az
+            Alma Mater Hostelben lesznek. A bejelentkezésre a kijelölt szálláshelyen van lehetőség!
+          </p>
+          <p>Parkolni a szállásokhoz bevezető út mentén, és az Alma Mater Hostel mögötti térségen lehet.</p>
+          <p>
+            Az ünnepélyes megnyitó és a záróünnepség az egyetemi sportcsarnok mögötti műfüves pályán kerül
+            megrendezésre.
+          </p>
+          <p>
+            Étkezés, főügyészi „verseny” és esti bulik helyszíne a CsabaPark Pálinkamúzeuma [Gyulai út 61/2. szám], ami
+            a szállásoktól pár perces sétával elérhető. A tájékozódást eligazító táblák segítik majd.
           </p>
           <p>
             Az atlétika versenyek – síkfutás és súlylökés - helyszíne a Békéscsabai Atlétika Club Tünde utca
@@ -53,12 +62,12 @@ export default function Page() {
           </p>
           <p>
             Labdarúgás: a Békéscsaba 1912 Előre Sporttelepén, a „Fű 2” műfüves pályán [Kórház utca 6. szám], az atlétika
-            pályától 2 perc séta. A tájékozódást eligazító táblák segítik majd. A pálya mellett büfé üzemel majd, és itt
-            lesz az egészségügyi ügyelet központja is.
+            pályától 2 perc séta. A tájékozódást eligazító táblák segítik. A pálya mellett büfé üzemel majd, és itt lesz
+            az egészségügyi ügyelet központja is.
           </p>
           <p>Asztalitenisz, kosárlabda, tollaslabda helyszíne a szállás mellett található egyetemi sportcsarnok.</p>
           <p>Az íjászat az egyetemi sportcsarnok mögötti füves térségen lesz.</p>
-          <p>Darts és csocsó a Csaba Parkban, az esti bulihelyszín melletti Kolbászmúzeumban.</p>
+          <p>Darts és csocsó a CsabaParkban, az esti bulihelyszín melletti Kolbászmúzeumban.</p>
           <p>
             Úszás az Árpád Gyógy- és Strandfürdőben [Árpád sor 3.], a szállástól kb. 20 perc gyalog. Bejárat az Árpád
             sori nyári főbejáratnál. A fürdőbe kizárólag az úszócsapatok tagjai léphetnek be díjmentesen, a neveket
@@ -128,12 +137,12 @@ export default function Page() {
           <Title id={`Asporttalálkozóidőbeosztása`}>
             <span data-counter>A sporttalálkozó időbeosztása</span>
           </Title>
-          <h3 className={`underline`}>2025. június 27., péntek</h3>
+          <h3 className={`underline`}>2025. június 27-e, péntek</h3>
           <p>
-            <span className={`font-bold`}>11:00 Érkezés, szállás elfoglalása.</span> Az Alma Mater Hostel aulájában a
-            csapatfelelősök fogadják a résztvevőket. A csapatok sportfelelőseinek fogjuk átadni az étkezési jegyeket,
-            valamint a karszalagokat is, amely nélkül nem lehet bejutni a kollégiumba, illetve az étkezés és az esti
-            rendezvényekre. A szobabeosztást és kulcsokat érkezéskor átadjuk.{' '}
+            <span className={`font-bold`}>11:00 Érkezés, szállás elfoglalása.</span> A kijelölt szálláshelyek aulájában
+            a csapatfelelősök fogadják a résztvevőket. A csapatok sportfelelőseinek fogjuk átadni az étkezési jegyeket,
+            valamint a karszalagokat is, amely nélkül nem lehet bejutni a kollégiumba, illetve az étkezésekre és az esti
+            rendezvényekre. A szobabeosztást és kulcsokat érkezéskor átadjuk.
           </p>
           <strong className={`font-bold`}>
             Figyelem! Kérjük, mindenki vigyázzon az átvett elektronikus kulcskártyákra, mert azokat – elvesztésük,
@@ -144,7 +153,7 @@ export default function Page() {
             földszinti sportközpontjában.
           </p>
           <p>
-            <span className={`font-bold`}>14:00 Ünnepélyes megnyitó</span>, Gál Ferenc Egyetem műfüves pályája
+            <span className={`font-bold`}>14:00 Ünnepélyes megnyitó</span> egyetemi sportcsarnok mögötti műfüves pálya
           </p>
           <p className={`font-bold`}>Sportversenyek 15:00 – 18:30, majd 20:30 órától</p>
           <p>Kezdési időpontok:</p>
@@ -156,9 +165,9 @@ export default function Page() {
             <li>20:30: főügyészi táncbemutató, Pálinkamúzeum</li>
             <li>21.00: darts, Kolbászmúzeum</li>
           </ul>
-          <h3 className={`underline`}>2025. június 28., szombat</h3>
+          <h3 className={`underline`}>2025. június 28-a, szombat</h3>
           <p>
-            10:00 – 12:00 óra között VIP vendégek számára: idegenvezetéses séta a Munkácsy negyedben. [Kizárólag a
+            10:00 – 12:00 óra között VIP vendégek számára: idegenvezetéses séta a Munkácsy Negyedben. [Kizárólag a
             meghívott vendégek számára szervezett, zártkörű program!]
           </p>
           <p className={`font-bold`}>Sportversenyek 09:00 – 12:00, 14:00 – 18:00, majd 20:00 órától</p>
@@ -171,7 +180,7 @@ export default function Page() {
             <li>14:30: asztalitenisz, kosárlabda, tollaslabda, sportcsarnok</li>
             <li>20:00: csocsó, Kolbászmúzeum</li>
           </ul>
-          <h3 className={`font-bold`}>2025. június 29., vasárnap</h3>
+          <h3 className={`font-bold`}>2025. június 29-e, vasárnap</h3>
           <p className={`font-bold`}>Sportversenyek 10:00 órától</p>
           <p className={`font-bold`}>Kezdési időpontok:</p>
           <ul>
@@ -197,47 +206,47 @@ export default function Page() {
             <tbody>
               <tr>
                 <td>Bács-Kiskun</td>
-                <td>dr. Kovács-Kulcsár Emese</td>
+                <td className={`inline-block max-w-full text-balance`}>dr. Kovács-Kulcsár Emese</td>
               </tr>
               <tr>
                 <td>Borsod-Abaúj-Zemplén</td>
-                <td>Gajdáné dr. Fehér Ildikó</td>
+                <td className={`inline-block max-w-full text-balance`}>Gajdáné dr. Fehér Ildikó</td>
               </tr>
               <tr>
                 <td>Csongrád-Csanád</td>
-                <td>dr. Kismarton Annamária</td>
+                <td className={`inline-block max-w-full text-balance`}>dr. Kismarton Annamária</td>
               </tr>
               <tr>
                 <td>Főváros</td>
-                <td>dr. Szelezsán Annamária</td>
+                <td className={`inline-block max-w-full text-balance`}>dr. Szelezsán Annamária</td>
               </tr>
               <tr>
                 <td>Hajdú-Bihar</td>
-                <td>dr. Molnár Edina</td>
+                <td className={`inline-block max-w-full text-balance`}>dr. Molnár Edina</td>
               </tr>
               <tr>
                 <td>Heves-Nógrád</td>
-                <td>Gyüre Tibor</td>
+                <td className={`inline-block max-w-full text-balance`}>Gyüre Tibor</td>
               </tr>
               <tr>
                 <td>Jász-Nagykun-Szolnok</td>
-                <td>dr. Futó Sándor</td>
+                <td className={`inline-block max-w-full text-balance`}>dr. Futó Sándor</td>
               </tr>
               <tr>
-                <td>KNYF</td>
-                <td>dr. Rozgonyi-Szabó Éva</td>
+                <td>Központi Nyomozó Főü.</td>
+                <td className={`inline-block max-w-full text-balance`}>dr. Rozgonyi-Szabó Éva</td>
               </tr>
               <tr>
                 <td>Legfőbb Ügyészség</td>
-                <td>Havasiné dr. Varga Anikó</td>
+                <td className={`inline-block max-w-full text-balance`}>Havasiné dr. Varga Anikó</td>
               </tr>
               <tr>
                 <td>Pest</td>
-                <td>dr. Pap Tamás</td>
+                <td className={`inline-block max-w-full text-balance`}>dr. Pap Tamás</td>
               </tr>
               <tr>
-                <td>Szabolcs</td>
-                <td>Gróh Tibor</td>
+                <td>Szabolcs-Szatmár-Bereg</td>
+                <td className={`inline-block max-w-full text-balance`}>Gróh Tibor</td>
               </tr>
             </tbody>
           </table>
@@ -255,43 +264,43 @@ export default function Page() {
             <tbody>
               <tr>
                 <td>asztalitenisz</td>
-                <td>Bónusné dr. Lehoczki Anett</td>
+                <td className={`inline-block max-w-full text-balance`}>Bónusné dr. Lehoczki Anett</td>
               </tr>
               <tr>
                 <td>csocsó</td>
-                <td>dr. Kismarton Annamária</td>
+                <td className={`inline-block max-w-full text-balance`}>dr. Kismarton Annamária</td>
               </tr>
               <tr>
                 <td>darts</td>
-                <td>dr. Herédi Kitti</td>
+                <td className={`inline-block max-w-full text-balance`}>dr. Herédi Kitti</td>
               </tr>
               <tr>
                 <td>íjászat</td>
-                <td>dr. Pap Tamás</td>
+                <td className={`inline-block max-w-full text-balance`}>dr. Pap Tamás</td>
               </tr>
               <tr>
                 <td>kosárlabda</td>
-                <td>dr. Molnár Edina</td>
+                <td className={`inline-block max-w-full text-balance`}>dr. Molnár Edina</td>
               </tr>
               <tr>
                 <td>labdarúgás</td>
-                <td>dr. Képiró Tibor</td>
+                <td className={`inline-block max-w-full text-balance`}>dr. Képiró Tibor</td>
               </tr>
               <tr>
                 <td>síkfutás</td>
-                <td>Havasiné dr. Varga Anikó</td>
+                <td className={`inline-block max-w-full text-balance`}>Havasiné dr. Varga Anikó</td>
               </tr>
               <tr>
                 <td>súlylökés</td>
-                <td>Gyüre Tibor</td>
+                <td className={`inline-block max-w-full text-balance`}>Gyüre Tibor</td>
               </tr>
               <tr>
                 <td>tollaslabda</td>
-                <td>Juhosné dr. Pleskó Ágnes</td>
+                <td className={`inline-block max-w-full text-balance`}>Juhosné dr. Pleskó Ágnes</td>
               </tr>
               <tr>
                 <td>úszás</td>
-                <td>Havasiné dr. Varga Anikó</td>
+                <td className={`inline-block max-w-full text-balance`}>Havasiné dr. Varga Anikó</td>
               </tr>
             </tbody>
           </table>
@@ -299,12 +308,13 @@ export default function Page() {
             <span data-counter>Étkezéssel kapcsolatos információk</span>
           </Title>
           <p>
-            Minden étkezés a szállással szomszédos telken található Csaba Park Rendezvényhelyszín területén található
-            Pálinkaházban lesz, ahová az étkezések idején{' '}
+            Minden étkezés a szállással szomszédos telken található CsabaPark Rendezvényhelyszín területén lévő
+            Pálinkamúzeumban lesz, ahová az étkezések idején{' '}
             <span className={`inline-block max-w-full text-balance underline`}>karszalaggal és étkezési jeggyel</span>{' '}
             rendelkező személyek mehetnek be.
           </p>
           <table>
+            <caption>Étkezések</caption>
             <thead>
               <tr>
                 <th colSpan={2}>Péntek:</th>
@@ -351,7 +361,6 @@ export default function Page() {
               </tr>
             </tbody>
           </table>
-
           <table>
             <caption>A menütervezet</caption>
             <thead>
@@ -362,7 +371,7 @@ export default function Page() {
             <tbody>
               <tr>
                 <td>vacsora</td>
-                <td>pankómorzsás rántott csirkemell, rizi-bizi</td>
+                <td>pankómorzsás rántott csirkemell, rizi-bizivel</td>
               </tr>
             </tbody>
             <thead>
@@ -383,7 +392,7 @@ export default function Page() {
               </tr>
               <tr>
                 <td>Vacsora</td>
-                <td>gödöllői töltött csirkecomb, burgonyapüré</td>
+                <td>gödöllői töltött csirkecomb burgonyapürével</td>
               </tr>
             </tbody>
             <thead>
@@ -398,7 +407,7 @@ export default function Page() {
               </tr>
               <tr>
                 <td>Ebéd</td>
-                <td>húsleves gazdagon csigatésztával; cigánypecsenye szalonna taréjjal, hagymás tört burgonya</td>
+                <td>húsleves gazdagon csigatésztával; cigánypecsenye szalonna taréjjal, hagymás tört burgonyával</td>
               </tr>
             </tbody>
           </table>
@@ -410,26 +419,51 @@ export default function Page() {
             <span data-counter>Esti programok, vendéglátás</span>
           </Title>
           <p>
-            Az esti programok helyszíne a Csaba Park Pálinkamúzeuma, a szállástól könnyed sétával megközelíthető. Az
+            Az esti programok helyszíne a CsabaPark Pálinkamúzeuma, a szállástól könnyed sétával megközelíthető. Az
             utcára sem kell kimenni, mert a 2 szomszédos telek közötti kerítést a rendezők a sporttalálkozó idejére
             kinyitják, és a közvilágítást is megoldják.
           </p>
           <p>
-            Pénteken az esti program a főügyészek táncbemutatójával indul, 20.30 órától. Előtte a helyszínt át kell
-            rendezni, ezért a vacsora 20.00 óráig vehető igénybe. 21.00 órától DISCO ugyanott, míg a mellette lévő
-            épületben, a Kolbászmúzeumban kerül sor a darts verseny lebonyolítására. A buli helyszínén – jó idő esetén –
-            kiülős terasz is lesz.
+            Pénteken az esti program a főügyészek táncbemutatójával indul, 20:30 órától, a csapatok buzdító részvételére
+            nagyon számítunk!
           </p>
-          <p>Szombat este 21.00 órától ismét DISCO, vele párhuzamosan, a darts helyszínén kerül sor a csocsótornára.</p>
+          <p>
+            Előtte a helyszínt át kell rendezni, ezért a vacsora 20:00 óráig vehető igénybe. 21:00 órától DISCO
+            ugyanott, míg a mellette lévő épületben, a Kolbászmúzeumban kerül sor a darts verseny lebonyolítására. A
+            buli helyszínén – jó idő esetén – kiülős terasz is lesz.
+          </p>
+          <p>Szombat este 21:00 órától ismét DISCO, vele párhuzamosan, a darts helyszínén kerül sor a csocsótornára.</p>
           <p>
             A buli helyszínén büfé üzemel majd, valamint ugyancsak lesz kitelepült büfé a labdarúgó torna helyszínén, a
             Kórház utcában. Az Alma Mater Hostel aulájában italautomata működik.
+          </p>
+          <p>
+            Az atlétika pályán, a labdarúgó pályán és az egyetemi sportcsarnokban a résztvevőknek ballonos vizet
+            biztosítunk.
           </p>
           <Title id={`Egészségügyibiztosítás`}>
             <span data-counter>Egészségügyi biztosítás</span>
           </Title>
           <p>A sportrendezvények idején gyalogos járőrös egészségügyi biztosítás lesz a helyszíneken.</p>
-          <p>Az egészségügyi biztosítás kontaktja: Varga Adrián.</p>
+          <p>
+            Az egészségügyi biztosítás kontaktja:{' '}
+            <span className={`inline-block max-w-full text-balance`}>Varga Adrián</span>.
+          </p>
+          <Title id={`Sporttalálkozóvalkapcsolatosinformációk`}>
+            <span data-counter>Sporttalálkozóval kapcsolatos információk</span>
+          </Title>
+          <p>A sporttalálkozó eredményei az Alma Mater Hostel aulájában kihelyezett monitoron követhetők.</p>
+          <p>
+            Az eseménnyel kapcsolatos valamennyi információ <span className={`underline`}>mobilapplikáción</span> is
+            elérhető a következő linken:
+          </p>
+          <ul>
+            <li>
+              <a target={`_blank`} href={`https://sport.martossy.hu`}>
+                https://sport.martossy.hu
+              </a>
+            </li>
+          </ul>
         </EntryContent>
       </Entry>
     </main>
