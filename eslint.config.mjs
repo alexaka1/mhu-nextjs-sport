a848c23bd1c4fd008ddc0defd9bb5738ddc7fecc
Martossy Alex <github@mail.martossy.hu>
2025-08-25 19:58:03 +0200

    Update ESLint and TypeScript configurations and refine lint commands

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,6 +17,9 @@ const config = ts.config(
   js.configs.recommended,
   ...tailwind.configs['flat/recommended'],
   eslintConfigPrettier,
+  {
+    ignores: ['node_modules/**', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
+  },
   ...compat.extends('next/core-web-vitals' /*, 'next/typescript'*/ /*added by ts below*/),
   ...ts.configs.strictTypeChecked,
   ...ts.configs.stylisticTypeChecked,
