4c41198a9d4c631170d1a6f3f94e995f98f2d4e4
Alex Martossy <github@mail.martossy.hu>
2025-10-30 16:47:56 +0100

    Merge migrations

--- drizzle/0004_gray_paladin.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE `account` ADD `createdAt` integer DEFAULT (cast(unixepoch('subsecond') * 1000 as integer)) NOT NULL;--> statement-breakpoint
-ALTER TABLE `account` ADD `updatedAt` integer DEFAULT (cast(unixepoch('subsecond') * 1000 as integer)) NOT NULL;--> statement-breakpoint
-ALTER TABLE `session` ADD `createdAt` integer DEFAULT (cast(unixepoch('subsecond') * 1000 as integer)) NOT NULL;--> statement-breakpoint
-ALTER TABLE `session` ADD `updatedAt` integer DEFAULT (cast(unixepoch('subsecond') * 1000 as integer)) NOT NULL;
--- drizzle/0005_ancient_screwball.sql
+++
@@ -1,5 +0,0 @@
-CREATE INDEX `account_userId_idx` ON `account` (`userId`);--> statement-breakpoint
-CREATE INDEX `session_userId_idx` ON `session` (`userId`);--> statement-breakpoint
-CREATE INDEX `session_token_idx` ON `session` (`sessionToken`);--> statement-breakpoint
-CREATE INDEX `user_email_idx` ON `user` (`email`);--> statement-breakpoint
-CREATE INDEX `verification_identifier_idx` ON `verificationToken` (`identifier`);
