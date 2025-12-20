2144f866c29a7caefbe5847b7a008b1d541665f6
Martossy Alex <github@mail.martossy.hu>
2025-12-20 15:42:20 +0100

    Update `LandingLink` to improve focus-visible styling in light mode

--- src/app/ui/buttons/landing-link.tsx
+++ src/app/ui/buttons/landing-link.tsx
@@ -11,7 +11,7 @@ export default function LandingLink({
   return (
     <Link
       href={href}
-      className="group relative inline-block rounded-lg border border-transparent px-5 py-4 text-white transition-colors duration-200 hover:border-gray-300 hover:bg-gray-100 not-dark:hover:text-black focus:border-gray-300 focus:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 focus:dark:border-neutral-700 focus:dark:bg-neutral-800/30"
+      className="group relative inline-block rounded-lg border border-transparent px-5 py-4 text-white transition-colors duration-200 hover:border-gray-300 hover:bg-gray-100 not-dark:hover:text-black focus:border-gray-300 focus:bg-gray-100 focus-visible:not-dark:text-black hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30 focus:dark:border-neutral-700 focus:dark:bg-neutral-800/30"
     >
       {children}
     </Link>
