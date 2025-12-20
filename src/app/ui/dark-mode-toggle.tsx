dae89c24a1599a5f8bc81acbae32ebddf3f203ee
Martossy Alex <github@mail.martossy.hu>
2025-12-20 13:54:52 +0100

    Remove unnecessary `asChild` prop from `DropdownMenuTrigger` in dark mode toggle component

--- src/app/ui/dark-mode-toggle.tsx
+++ src/app/ui/dark-mode-toggle.tsx
@@ -17,12 +17,10 @@ export function DarkModeToggle() {
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild={true}>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
-          <span className="sr-only">Téma váltás</span>
-        </Button>
+      <DropdownMenuTrigger>
+        <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+        <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+        <span className="sr-only">Téma váltás</span>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
         <DropdownMenuItem
